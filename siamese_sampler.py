d7d5644b193e51a75baaf6d69bb67077e6d66769
Alexander Becker <alexander.becker@netfira.com>
2020-01-24 10:20:21 +0100

    Refactor

--- siamese_sampler.py
+++ siamese_sampler.py
@@ -1,55 +1,13 @@
 import random
 
 import numpy as np
-import torch
 from torch.utils.data.sampler import Sampler
 
 
-def get_positive_mask(ids):
-    ids = ids.unsqueeze(0)
-    mask = ids == ids.t()
-    idx = torch.arange(ids.shape[1])
-    mask[idx, idx] = 0
-    return mask.float()
-
-
-def get_negative_mask(ids):
-    ids = ids.unsqueeze(0)
-    mask = ids != ids.t()
-    return mask.float()
-
-
-def select_triplets(embeddings, ids):
-    ids = ids.detach()
-    # calculate distance matrix
-    dot = torch.mm(embeddings, embeddings.t()).detach()
-    square_norm = dot.diagonal()
-    # ||a - b||^2 = ||a||^2  - 2 <a, b> + ||b||^2
-    dist = square_norm.expand_as(dot).t() - 2 * dot + square_norm.expand_as(dot)
-    dist = dist.clamp(min=0)
-    
-    # get hardest positive for each embedding
-    pos_mask = get_positive_mask(ids)
-    pos_dist = dist.clone()
-    pos_dist *= pos_mask
-    pos_indices = pos_dist.argmax(dim=1)
-    
-    # hardest negative
-    neg_mask = get_negative_mask(ids)
-    neg_dist = dist.clone()
-    neg_dist[neg_mask == 0.] += neg_dist.max()
-    neg_indices = neg_dist.argmin(dim=1)
-    
-    positives = embeddings[pos_indices].contiguous()
-    negatives = embeddings[neg_indices].contiguous()
-    
-    return embeddings, positives, negatives
-
-
-class BatchHardSampler(Sampler):
+class SiameseSampler(Sampler):
     """
-    Samples by the batch hard strategy (See https://omoindrot.github.io/triplet-loss#batch-hard-strategy)
-    The batch size is defined as num_ids * num_samples.
+    Samples batches containing num_ids identities with num_samples samples each.
+    Thus, the batch size is defined as num_ids * num_samples.
     
     :param dataset: the dataset to sample over
     :param num_ids: number of disctinct identities to be included in one batch
@@ -59,7 +17,7 @@ class BatchHardSampler(Sampler):
     """
     
     def __init__(self, dataset, num_ids, num_samples, validate=False, validation_size=0.2):
-        super(BatchHardSampler, self).__init__(dataset)
+        super(SiameseSampler, self).__init__(dataset)
         
         self.dataset = dataset
         self.num_ids = num_ids
@@ -71,7 +29,6 @@ class BatchHardSampler(Sampler):
             self.ids_unique = dataset.ids_unique[:split]
         else:
             self.ids_unique = dataset.ids_unique[split:]
-            
             
         self.id_sample_map = {_id : np.where(self.ids == _id)[0] for _id in self.ids_unique}
         
---
+++ triplet_selection.py
@@ -0,0 +1,47 @@
+import numpy as np
+import torch
+
+
+def _get_positive_mask(ids):
+    ids = ids.unsqueeze(0)
+    mask = ids == ids.t()
+    idx = torch.arange(ids.shape[1])
+    mask[idx, idx] = 0
+    return mask.float()
+
+
+def _get_negative_mask(ids):
+    ids = ids.unsqueeze(0)
+    mask = ids != ids.t()
+    return mask.float()
+
+
+def select_triplets(embeddings, ids):
+    """
+    Select embedding triplets according to the batch hard strategy
+    (https://omoindrot.github.io/triplet-loss#batch-hard-strategy).
+    """
+    ids = ids.detach()
+    # calculate distance matrix
+    dot = torch.mm(embeddings, embeddings.t()).detach()
+    square_norm = dot.diagonal()
+    # ||a - b||^2 = ||a||^2  - 2 <a, b> + ||b||^2
+    dist = square_norm.expand_as(dot).t() - 2 * dot + square_norm.expand_as(dot)
+    dist = dist.clamp(min=0)
+    
+    # get hardest positive for each embedding
+    pos_mask = _get_positive_mask(ids)
+    pos_dist = dist.clone()
+    pos_dist *= pos_mask
+    pos_indices = pos_dist.argmax(dim=1)
+    
+    # hardest negative
+    neg_mask = _get_negative_mask(ids)
+    neg_dist = dist.clone()
+    neg_dist[neg_mask == 0.] += neg_dist.max()
+    neg_indices = neg_dist.argmin(dim=1)
+    
+    positives = embeddings[pos_indices].contiguous()
+    negatives = embeddings[neg_indices].contiguous()
+    
+    return embeddings, positives, negatives
